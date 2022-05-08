36cb199fc74b786ecab4c878c0ac2479b9d7ecd7
KingLinus <46587872+KingLinus@users.noreply.github.com>
2022-05-08 19:13:40 +0000

    Added toxic command

---
+++ commands/toxic.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'toxic',
+	description: 'Toxic',
+	execute(message) {
+		message.channel.send('https://media.discordapp.net/attachments/637834285327450131/971842838503489616/unknown.png');
+	},
+};
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 //Dependancies
 const fs = require('fs');
 const Discord = require('discord.js');
