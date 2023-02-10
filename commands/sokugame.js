67e785e279f350851de24c3779205654feab6e0f
KingLinus <46587872+KingLinus@users.noreply.github.com>
2023-02-10 02:48:44 +0000

    updated soku link

--- commands/sokugame.js
+++ commands/sokugame.js
@@ -3,6 +3,6 @@ module.exports = {
     description: 'soku game download link',
     aliases: ['soku'],
 	execute(message) {
-		message.channel.send('http://www.mediafire.com/file/7ulje2arv05zjbg/Touhou%20Hisoutensoku%20+%20Full%20Unlock%20+%20SokuRoll%20+%20SWRSToys%20+%20Tensokukan.zip');
+		message.channel.send('https://mega.nz/file/0hgCma6I#zLYPYi4BtpZ_N_Hpp42VKugY5R9fMLuhao5rDcayVxM');
 	},
 };
